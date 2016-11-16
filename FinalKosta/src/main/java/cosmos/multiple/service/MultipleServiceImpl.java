3578201799740c96c00ce343b33f0eb0193fc3be
kibeom <sco009@naver.com>
2016-11-16 10:31:43 +0900

    2016.11.16-2(pkb)

--- FinalKosta/src/main/java/cosmos/multiple/service/MultipleServiceImpl.java
+++ FinalKosta/src/main/java/cosmos/multiple/service/MultipleServiceImpl.java
@@ -4,9 +4,16 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
+import cosmos.multiple.persistence.MultipleDAO;
+
 @Service
 public class MultipleServiceImpl implements MultipleService {
 
 	@Inject
-	private 
+	private MultipleDAO dao;
+	
+	@Override
+	public void re(){
+		
+	}
 }
