ece0cadbd0da7a4f287633d3a38389421dce857a
HongSuBin <me_saw@hanmail.net>
2016-11-17 10:10:13 +0900

    2016.11.17-2 hsb

--- FinalKosta/src/main/java/org/zerock/controller/HomeController.java
+++ FinalKosta/src/main/java/org/zerock/controller/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		//hi
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
