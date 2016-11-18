6e6f0140ca7b7172e1a0bb21a0aba240c9286492
HongSuBin <me_saw@hanmail.net>
2016-11-18 09:40:01 +0900

    2016.11.18-1HSB

--- FinalKosta/src/main/java/cosmos/ranking/service/RankingServiceImpl.java
+++ FinalKosta/src/main/java/cosmos/ranking/service/RankingServiceImpl.java
@@ -1,5 +1,39 @@
 package cosmos.ranking.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import cosmos.ranking.domain.AlgoRankingVO;
+import cosmos.ranking.domain.CodeRankingVO;
+import cosmos.ranking.persistence.RankingDAO;
+
+@Service
 public class RankingServiceImpl implements RankingService {
+	@Inject
+	private RankingDAO dao;
+	
+	@Override
+	public List<AlgoRankingVO> allAlgoRanking() {
+		return dao.allAlgoRanking();
+	}
+
+	@Override
+	public List<CodeRankingVO> allCodeRanking() {
+		return dao.allCodeRanking();
+	}
+
+	@Override
+	public void updateAlgoRanking(AlgoRankingVO algoVO) {
+		dao.updateAlgoRanking(algoVO);
+	}
+
+	@Override
+	public void updateCodeRanking(CodeRankingVO codeVO) {
+		dao.updateCodeRanking(codeVO);
+	}
+
 
 }
