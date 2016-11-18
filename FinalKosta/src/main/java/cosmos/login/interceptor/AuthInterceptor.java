667fde2addf8e9e6011a609a796241e318cdad0f
AB <AB@ANBIN-PC>
2016-11-18 16:47:16 +0900

    2016.11.18-6KAB

--- FinalKosta/src/main/java/cosmos/login/interceptor/AuthInterceptor.java
+++ FinalKosta/src/main/java/cosmos/login/interceptor/AuthInterceptor.java
@@ -5,9 +5,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.springframework.ui.ModelMap;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.util.WebUtils;
 
