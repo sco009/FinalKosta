a21928c43d6fb96cea7ae7a363c4eda7d7f8d90c
Byungjin <Byungjin@DESKTOP-PE1NF9A>
2016-11-18 16:54:09 +0900

    KBJ 2016-11-18

--- FinalKosta/src/main/java/cosmos/multiple/persistence/MultipleDAO.java
+++ FinalKosta/src/main/java/cosmos/multiple/persistence/MultipleDAO.java
@@ -1,5 +1,19 @@
 package cosmos.multiple.persistence;
 
+import java.util.List;
+
+import cosmos.multiple.domain.MultipleChoice;
+import cosmos.multiple.domain.MultiplePoint;
+import cosmos.multiple.domain.MultipleVO;
+
 public interface MultipleDAO {
 
+	public List<MultipleVO> selectMultiple(MultipleVO vo) throws Exception;
+
+	public MultipleChoice selectMultipleChoice(String multipleChoiceId) throws Exception;
+
+	public int selectMultipleCount(MultipleVO vo) throws Exception;
+
+	public MultipleVO reMultiple(String mulquestId) throws Exception;
+	//public int pointInsert(MultiplePoint multiplePoint) throws Exception;
 }
--- FinalKosta/src/main/java/cosmos/multiple/service/MultipleService.java
+++ FinalKosta/src/main/java/cosmos/multiple/service/MultipleService.java
@@ -1,5 +1,19 @@
 package cosmos.multiple.service;
 
-public interface MultipleService {
+import java.util.List;
 
+import cosmos.multiple.domain.MultipleChoice;
+import cosmos.multiple.domain.MultiplePoint;
+import cosmos.multiple.domain.MultipleVO;
+
+public interface MultipleService {
+	public List<MultipleVO> selectMultiple(MultipleVO vo) throws Exception;
+
+	public MultipleChoice selectMultipleChoice(String multipleChoiceId) throws Exception;
+
+	public int selectMultipleCount(MultipleVO vo) throws Exception;
+
+	public MultipleVO reMultiple(String mulquestId) throws Exception;
+
+//	public int pointInsert(MultiplePoint multiplePoint) throws Exception;
 }
