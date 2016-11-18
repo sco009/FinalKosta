60986de861de68a3aa56e8afe64d96d55c31c038
김치영 <@DESKTOP-SU5Q976>
2016-11-18 12:26:53 +0900

    2016.11.18-1(KCY)

--- FinalKosta/src/main/java/cosmos/education/controller/EducationController.java
+++ FinalKosta/src/main/java/cosmos/education/controller/EducationController.java
@@ -1,5 +1,15 @@
 package cosmos.education.controller;
 
-public class EducationController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class EducationController {
+	
+	@RequestMapping("education")
+	public String education(){
+		
+		return "/education/education_Main";
+	}
+	
 }
